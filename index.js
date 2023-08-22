24b45bb8baa9ee7653ef74d3a27ee11324cfdef7
emeralddd <tunglambinbin@gmail.com>
2023-08-23 01:52:11 +0700

    acbsc

--- index.js
+++ index.js
@@ -11,19 +11,27 @@ const testcases = dom.window.document.querySelectorAll("a");
 
 const convert = (i) => {
     const digits = 3;
-    let number = String(Math.floor(i / 3));
+    let number = String(Math.floor(i / 2));
     while (number.length !== digits) number = "0" + number;
     return number;
 }
 
+const req = [];
+
 for (let i = 0; i < testcases.length; i++) {
-    if (i % 3 === 1) continue;
-    const filename = "testcases/test" + convert(i) + (i % 3 === 0 ? '.inp' : '.out');
-    // console.log(filename);
-    request(testcases[i].href, function (error, response, html) {
+    if (!testcases[i].href) continue;
+    if (testcases[i].href.includes('temporary')) continue;
+    req.push(testcases[i].href);
+}
+
+for (let i = 0; i < req.length; i++) {
+    request(req[i], function (error, response, html) {
         if (!error && response.statusCode == 200) {
+            const filename = "testcases/test" + convert((i)) + ((i) % 2 === 0 ? '.inp' : '.out');
+            // console.log(i, req[i], filename);
             writeFileSync(filename, html);
-            // console.log(html);
+        } else {
+            console.log(error);
         }
     });
 }
